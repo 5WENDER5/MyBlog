b7eb17d27d8d3ef91fee9f8003e2d93082630fa6
5WENDER5 <1458151865@qq.com>
2020-05-16 01:18:33 +0800

    second-one

--- src/main/java/com/example/blog/web/IndexController.java
+++ src/main/java/com/example/blog/web/IndexController.java
@@ -13,6 +13,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class IndexController {
@@ -34,5 +36,15 @@ public class IndexController {
         model.addAttribute("tags", tagService.listTagTop(10));
         model.addAttribute("recommendBlogs", blogService.listRecommendBlogTop(8));
         return "index";
+    }
+
+    @PostMapping("/search")
+    public String search(@PageableDefault(size = 10, sort = {"updateTime"}, direction = Sort.Direction.DESC) Pageable pageable,
+                         @RequestParam String query, BlogQuery blog, Model model) {
+
+        model.addAttribute("page", blogService.listBlog("%" + query + "%", pageable));
+        model.addAttribute("query", query);
+
+        return "search";
     }
 }
